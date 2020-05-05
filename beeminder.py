94716768d067cd524a7114ad8021ba778543450c
Dominik Stańczak <stanczakdominik@gmail.com>
2020-05-05 15:27:09 +0200

    Formatting, refactor

--- beeminder.py
+++ beeminder.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """Utility script for CLI Beeminder usage.
 
 Example usage/API design doc draft:
@@ -13,45 +14,56 @@ updates; asks for description
 >>> beeminder books 1 "#PlasmaWaves"
 updates; asks for nothing
 >>> beeminder jrnl
-this is an api goal; checks for registered handlers, applies them; can be called via systemctl assuming secrets are provided...
+this is an api goal; checks for registered handlers, applies them;
+can be called via systemctl assuming secrets are provided...
 >>> beeminder todoist
 this is an external goal; displays useful information
 >>> beeminder todoist edit
 """
-
-
-#!/usr/bin/env python
 import requests
 from datetime import datetime
 import click
 import os
-from collections import namedtuple
 
 username = os.environ["BEEMINDER_USERNAME"]
-auth = {"username":username,"auth_token":os.environ["BEEMINDER_TOKEN"]}
+auth = {"username": username, "auth_token": os.environ["BEEMINDER_TOKEN"]}
 goals = []
 url = f"https://www.beeminder.com/api/v1/users/{username}/goals.json"
 r = requests.get(url, params=auth).json()
 
+
 class Goal:
+    """Wraps a Beeminder goal."""
+
     def __init__(self, **goal):
-        self.losedate = goal['losedate']
-        self.slug = goal['slug']
-        self.limsum = goal['limsum']
-        self.title = goal['title']
+        """TODO."""
+        self.losedate = goal["losedate"]
+        self.slug = goal["slug"]
+        self.limsum = goal["limsum"]
+        self.title = goal["title"]
+
+    @property
+    def formatted_losedate(self):
+        date = datetime.utcfromtimestamp(self.losedate)
+        return date.strftime("%Y-%m-%d %H:%M:%S")
+
+    @property
+    def summary(self):
+        ts = self.formatted_losedate
+        return f"{self.slug.upper():18}{self.limsum:27} derails at {ts:25}{self.title}"
+
 
 for goal in r:
     goal = Goal(**goal)
     goals.append(goal)
 
-@click.command()
-def show_timings():
-    """
-    Display timings for beeminder goals
-    """
-    for goal in sorted(goals, key = lambda x: x.losedate):
-        ts = datetime.utcfromtimestamp(goal.losedate).strftime('%Y-%m-%d %H:%M:%S')
-        print(f"{goal.slug.upper():18}{goal.limsum:27} derails at {ts:25}{goal.title}")
 
-if __name__ == '__main__':
+@click.command()
+def show_timings(manual = False):
+    """Display timings for beeminder goals."""
+    for goal in sorted(goals, key=lambda g: g.losedate):
+        click.echo(goal.summary)
+
+
+if __name__ == "__main__":
     show_timings()
